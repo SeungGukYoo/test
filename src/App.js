0ffaf22725c79d1c0bc82f5555b867c2c9f20b50
SeungGukYoo <dbtmd878@gmail.com>
2024-01-08 22:28:51 +0900

    home component add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>commit 1</h1>
+        <h2>commit 2</h2>
+        <h3>commit 3</h3>
+        <h4>commit 4</h4>
+        <h5>commit 5</h5>
+        <span>commit 6</span>
+        <p>develop branch</p>
       </header>
+      <Home />
     </div>
   );
 }
